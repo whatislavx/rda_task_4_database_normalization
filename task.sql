42659996ac442a443fee45ac1f75eb4c57db8101
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2025-11-13 00:35:28 +0200

    Solution v3

--- task.sql
+++ task.sql
@@ -13,30 +13,38 @@ CREATE TABLE WareHouses (
     ID INT,
     WarehouseName VARCHAR(50),
     WarehouseAddress VARCHAR(50),
+    CountryID INT,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Products (
+	ID INT,
+    ProductName VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE ProductInventory (
     ID INT,
-    ProductName VARCHAR(50),
+    ProductID INT,
     WarehouseID INT,
     WarehouseAmount INT,
-    CountryID INT,
-    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
     FOREIGN KEY (WarehouseID) REFERENCES WareHouses(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
 INSERT INTO Countries (ID, Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID, Name)
-	VALUES (2, 'Country2');
+	VALUES (1, 'Country1'),
+	       (2, 'Country2');
     
-INSERT INTO WareHouses (ID, WarehouseAmount, WarehouseName, WarehouseAddress)
-VALUES (1, 100, 'Main Warehouse', 'Address 1'),
-       (2, 200, 'Backup Warehouse', 'Address 2');
+INSERT INTO WareHouses (ID, WarehouseName, WarehouseAddress, CountryID)
+	VALUES (1, 'Main Warehouse', 'Address 1', 1),
+           (2, 'Backup Warehouse', 'Address 2', 2);
     
-INSERT INTO ProductInventory (ID, ProductName, CountryID, WarehouseID)
-	VALUES (1, 'AwersomeProduct', 1, 1);
-INSERT INTO ProductInventory (ID, ProductName, CountryID, WarehouseID)
-	VALUES (2, 'AwersomeProduct', 2, 2);
+INSERT INTO Products (ID, ProductName)
+	VALUES (1, 'AwersomeProduct'),
+		   (2, 'AwersomeProduct');
+    
+INSERT INTO ProductInventory (ID, ProductID, WarehouseID, WarehouseAmount)
+	VALUES (1, 1, 1, 100),
+	       (2, 2, 2, 200);
